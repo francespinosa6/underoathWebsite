7f94a0dc5042219d719c266e0e070c6a70517c1c
Francisco <franc.espinosa6@gmail.com>
2020-09-30 23:39:44 -0400

    fixed the color of the page when side menu closes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-function openNav() {                                                   //to open sidebar
+function openNav() {                                                   //to open side nav
     document.getElementById("mySidenav").style.width = "200px";
-    document.getElementById("main").style.marginRight = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
-  function closeNav() {                                               //to close sidebar
+  function closeNav() {                                               //to close side nav
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginRight= "0";
-    document.body.style.backgroundColor = "white";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "black";
   }
