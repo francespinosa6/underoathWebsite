1e61cc4dfb11870b048eaecf57a6d09e7d5c81ae
Francisco <franc.espinosa6@gmail.com>
2020-10-09 13:41:58 -0400

    Added functions to open and close the nav menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function openNav() {                                                   //to open side nav
     document.getElementById("mySidenav").style.width = "200px";
     document.getElementById("main").style.marginLeft = "200px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.body.style.backgroundColor = "black";
   }
   
   function closeNav() {                                               //to close side nav
